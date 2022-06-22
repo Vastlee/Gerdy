ccfcc905acd2d32a8ec8fa4ad6c068979f30729e
Vast <Vastlee@gmail.com>
2022-06-22 13:37:08 -0700

    Moved the Bot over into it's own class.

---
+++ Gerdy.App/Bot.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Discord;
+using Discord.WebSocket;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Gerdy.App;
+internal class Bot {
+    static DiscordSocketClient? client;
+
+    static public async Task RunAsync() {
+        const string configFileName = "GerdyConfig.json";
+        string botConfigFile = string.Empty;
+
+        using(FileStream fs = File.OpenRead($"{configFileName}")) {
+            using var sr = new StreamReader(fs, new UTF8Encoding(false));
+            botConfigFile = await sr.ReadToEndAsync().ConfigureAwait(false);
+        }
+
+        DiscordBotConfig tokenFile = JsonSerializer.Deserialize<DiscordBotConfig>(botConfigFile);
+
+        string token = tokenFile.Token;
+        
+        client = new DiscordSocketClient();
+        client.Log += Log;
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+        await Task.Delay(-1);
+    }
+    
+    static Task Log(LogMessage msg) {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
--- Gerdy.App/Program.cs
+++ Gerdy.App/Program.cs
@@ -1,36 +1,9 @@
-﻿using System.Text;
-using Discord;
-using Discord.WebSocket;
-using System.Text.Json;
+﻿using Gerdy.App;
 
 public class Program {
-    private DiscordSocketClient? client;
-    
     public static Task Main(string[] args) => new Program().MainAsync();
-    
-    public async Task MainAsync() {
-        const string configFileName = "GerdyConfig.json";
-        string botConfigFile = string.Empty;
-        
-        using(FileStream fs = File.OpenRead($"{configFileName}")) {
-            using var sr = new StreamReader(fs, new UTF8Encoding(false));
-            botConfigFile = await sr.ReadToEndAsync().ConfigureAwait(false);
-        }
 
-        DiscordBotConfig tokenFile = JsonSerializer.Deserialize<DiscordBotConfig>(botConfigFile);
-        
-        string token = tokenFile.Token;
-        
-        client = new DiscordSocketClient();
-        client.Log += Log;
-                
-        await client.LoginAsync(TokenType.Bot, token);
-        await client.StartAsync();
-        await Task.Delay(-1);
-    }
-    
-    private Task Log(LogMessage msg) {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
+    public async Task MainAsync() {
+        await Bot.RunAsync();
     }
 }
